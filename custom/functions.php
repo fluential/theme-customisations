2423946b9a75bcca0a93898edd191230bfa0703c
Mad Mike <fluential@users.noreply.github.com>
2019-08-17 09:07:48 +0100

    Update functions.php

--- custom/functions.php
+++ custom/functions.php
@@ -18,7 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 function company_customlogo() {
         add_theme_support( 'custom-logo', array(
              'width'       => 400,
-			'height'      => 120,
+	     'height'      => 120,
+	     'flex-width'  => true,
+	     'flex-height' => true
         ) );
 }
 add_action( 'after_setup_theme', 'company_customlogo', 11 );
